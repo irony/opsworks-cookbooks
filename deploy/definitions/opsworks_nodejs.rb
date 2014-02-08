31d95a5826f1e69d06c4eff4f71120b2e99c95da
irony <cl@iteam.se>
2014-02-07 16:52:50 -0800

    index as default name instead of server.js

--- deploy/definitions/opsworks_nodejs.rb
+++ deploy/definitions/opsworks_nodejs.rb
@@ -12,13 +12,13 @@ define :opsworks_nodejs do
     end
   end
 
-  template "#{deploy[:deploy_to]}/shared/config/opsworks.js" do
+  template "#{deploy[:deploy_to]}/shared/node_modules/config/index.js" do
     cookbook 'opsworks_nodejs'
     source 'opsworks.js.erb'
     mode '0660'
     owner deploy[:user]
     group deploy[:group]
-    variables(:database => deploy[:database], :memcached => deploy[:memcached], :layers => node[:opsworks][:layers])
+    variables(:deploy => deploy, :layers => node[:opsworks][:layers])
   end
 
   template "#{node.default[:monit][:conf_dir]}/node_web_app-#{application}.monitrc" do
@@ -30,7 +30,7 @@ define :opsworks_nodejs do
     variables(
       :deploy => deploy,
       :application_name => application,
-      :monitored_script => "#{deploy[:deploy_to]}/current/server.js"
+      :monitored_script => "#{deploy[:deploy_to]}/current/index.js"
     )
     notifies :restart, "service[monit]", :immediately
   end
